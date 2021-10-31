ca930faaaa83c3069e9131b08a3b2b2936b747f5
Chris Risden <christopherrisden@gmail.com>
2021-10-30 21:36:03 -0700

    layout for app in html/css - need to add functionality in js - going to start with page initialization to replicate what i currently have in html

---
+++ src/initializePage.js
@@ -0,0 +1,30 @@
+//here we will set up our page layout and then everything will be based on that!
+
+class pageLayout {
+  constructor() {
+
+  }
+
+  setProjectsHeader(projectName) {
+
+  }
+
+  setProjectTitle() {
+
+  }
+
+  setProjectList() {
+
+  }
+
+  setTodaysTasks() {
+
+  }
+
+
+
+
+
+
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
   mode: 'development',
   devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development'
-     }),
-  ],
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
