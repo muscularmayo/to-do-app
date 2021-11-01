658621d50569bb7546c18a68a91d6f12b8ed370d
Chris Risden <christopherrisden@gmail.com>
2021-10-31 22:04:26 -0700

    structuring comments

--- src/initializePage.js
+++ src/initializePage.js
@@ -1,27 +1,32 @@
 //here we will set up our page layout and then everything will be based on that!
 
 class pageLayout {
-  constructor() {
-
-  }
+  /*constructor() {
+    not necessary, just included to test - perhaps i'll find a use for this
+  }*/
 
   setProjectsHeader(projectName) {
-
+    //upper left: "Projects"
   }
 
   setProjectTitle() {
-
+    //upper right: this will be set based on the current project we're looking at
+    //
   }
 
   setProjectList() {
+    //bottom left: a list of all our projects
+    //we have a storage object, all of the keys on that object represent our project list
+  }
 
+  setTaskList() {
+    //bottom right: a list of all the tasks associated with this specific project of ours
+    //we will pull specificProject.tasks.forEach(title and date on a div and append to)
   }
 
   setTodaysTasks() {
 
   }
-
-
 
 
 
