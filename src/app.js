7fcd61c4644f3aaf4e72a4e0ec9e8974361e0316
Chris Risden <christopherrisden@gmail.com>
2021-11-01 17:48:44 -0700

    just more preliminary structuring... a bit clueless on how to structure

--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
 import './style.css';
+import pageInit from './initializePage'
+/* storage structure will look like:
+
+{project1: {tasks: [{title , date , details , priority}, {title , date , details , priority}]
+            description: 'a short description of our project here'                          },
+ project2: {tasks: [{array of objects, each object describing a single task}]
+            description: 'a short description of our project here'                          }
+}
+*/
+
+const storage = {}
+storage['default project'] =
+{'tasks' : [{'title': 'placeholder', 'date': '03/03/03', 'details': 'some deets here', 'priority': 'high'}],
+'description': 'a brief description of the project at hand'};
+
+
+
+
+
+
+
 
