996eaf2808c3f5da0919aab5cc37559901d37c58
Chris Risden <christopherrisden@gmail.com>
2021-11-05 18:02:03 -0700

    we are starting with the form and working back from there

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 import './style.css';
-import {initializePage} from './initializePage.js'
-
 
 
 /* storage structure will look like:
@@ -18,16 +16,117 @@ storage['default project'] =
 'description': 'a brief description of the project at hand'};
 */
 
+const addTaskButton = document.querySelector('#addTask');
+const container = document.querySelector('#container');
+const projects = document.querySelectorAll('.project');
+const tasks = document.querySelectorAll('.task');
+
+//addTaskButton.addEventListener('click', 'appendInputForm')
+
+
+
+
+
+const createTaskFromInput = function() {
+  const task = {}
+  task.title = document.querySelector('#title')
+  task.description = document.querySelector('#description')
+  task.date = document.querySelector('#date')
+  task.priority = document.querySelector('#priority')
+
+  return task;
+}
+
+const createInputForm = function() {
+  const main = document.createElement('div')
+  main.id = 'taskForm'
+
+  const titleLabel = document.createElement('label')
+  titleLabel.setAttribute('for','title')
+  titleLabel.innerHTML = 'Task Name'
+
+  const titleInput = document.createElement('input')
+  titleInput.setAttribute('type','text')
+  titleInput.setAttribute('id','title')
+
+  main.appendChild(titleLabel);
+  main.appendChild(titleInput);
+
+  const dateInput = document.createElement('input')
+  dateInput.setAttribute('type','date')
+  dateInput.setAttribute('id','date')
+  main.appendChild(dateInput);
+
+  const priorityLabel = document.createElement('label')
+  priorityLabel.setAttribute('for','priority')
+  priorityLabel.innerHTML = 'Priority'
+  const prioritySelect = document.createElement('select')
+  prioritySelect.setAttribute('id','priority')
+  prioritySelect.setAttribute('name', 'priority')
+  const highOption = document.createElement('option');
+  highOption.setAttribute('value','high')
+  const mediumOption = document.createElement('option')
+  mediumOption.setAttribute('value', 'medium')
+  const lowOption = document.createElement('option')
+  lowOption.setAttribute('value','low')
+
+  prioritySelect.appendChild(highOption)
+  prioritySelect.appendChild(mediumOption)
+  prioritySelect.appendChild(lowOption)
+
+  main.appendChild(prioritySelect)
+
+  const descriptionLabel = document.createElement('label')
+  descriptionLabel.setAttribute('for','description')
+  descriptionLabel.innerHTML = 'Task Description'
+  const descriptionInput = document.createElement('input')
+  descriptionInput.setAttribute('type','text')
+  descriptionInput.setAttribute('id', 'description')
+
+  main.appendChild(descriptionLabel)
+  main.appendChild(descriptionInput)
+
+  const div = document.createElement('div');
+  const submit = document.createElement('input')
+  submit.setAttribute('type', 'submit')
+  submit.setAttribute('value', 'Submit')
+  div.appendChild(submit)
+
+  main.appendChild(div);
+  //submit.setAttribute('onsubmit', 'createTaskFromInput')
+
+
+  return main;
+}
+
+console.log(createInputForm())
+
+
+/*<div id='taskForm'>
+          <label for='title'>Task Name</label>
+          <input type='text' id='title'>
+
+          <input type='date' id='date'>
+
+          <label for='priority'>Priority</label>
+          <select id='priority' name='priority'>
+            <option value='high'>High</option>
+            <option value='medium'>Medium</option>
+            <option value='low'>Low</option>
+          </select>
+
+          <label for='description'>Task Description</label>
+
+          <input type='text' id='description'>
+          <div>
+            <input type='submit' value='Submit' onsubmit='createTaskfromInput()'>
+          </div>
+        </div>
+*/
 
 
 
 
 
 
-
-
-
-
-
-
-export const container = document.querySelector('#container')
+export {container}
