ee900a3f0b35dba4e102126b64e3e8ec9c457716
Chris Risden <christopherrisden@gmail.com>
2022-01-13 16:07:25 -0800

    trying to bring the images back to ghpages

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    // publicPath: "/dist/index.html"
+    publicPath: '/dist/index.html',
   },
   module: {
     rules: [
